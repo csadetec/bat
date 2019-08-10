a8ca1866a1e3b45899576cc2d0f4264d0dad6acf
csadetec <informaticact@santoagostinho.com.br>
2019-08-10 10:18:01 -0300

    meu primeiro bat

---
+++ teste.bat
@@ -0,0 +1,22 @@
+:Dentro da pasta de cada autor cria pastas excel, word, power point.
+:E depois move os tipos de arquivos para cada pasta
+for /f "tokens=*" %a in (lista.txt) do (
+	cd %a
+	md excel, word, "power point"
+	move *.xlsx excel
+	move *.xls excel
+	move *.docx word
+	move *.doc word
+	move *.pptx "power point"
+	move *.ppt "power point"
+	cd /
+)
+:copy as pasta do hd, para o servidor de arquivos
+:e depois move as pasta para pasta ja_passei_publico
+for /f "tokens=*" %a in (lista.txt) do (
+	md \\10.10.0.66\dados\temp\publico\"%a"
+	xcopy /E "%a" \\10.10.0.66\dados\temp\publico\"%a"\
+	move "%a" ja_passei_publico
+	
+)
+
