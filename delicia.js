0fd8ca3410de56f20ab81cd6b5114b7d5d7c5676
csadetec <informaticact@santoagostinho.com.br>
2019-08-14 11:36:34 -0300

    testar essa delicia, pois separei as funcoes

--- delicia.js
+++ delicia.js
@@ -4,48 +4,72 @@ const fs = require('fs')
 const workbook = xlsx.readFile('autores.xlsx')
 const sheet = workbook.SheetNames
 
-var autor = xlsx.utils.sheet_to_json(workbook.Sheets[sheet[0]])
+var autores = xlsx.utils.sheet_to_json(workbook.Sheets[sheet[0]])
 
-//make folder
-if(!fs.existsSync(`./autores/sem_autores`)){
-	fs.mkdirSync(`./autores/sem_autores`)	
-}
-/** */
-for(r in autor){
-	let a = autor[r].nome
-	//a = a.replace('//', '')
-	
-	if(!fs.existsSync(`./autores/${a}`)){
-		fs.mkdirSync(`./autores/${a}`)	
-		//console.log(a)
-		//var res = str.replace("Microsoft", "W3Schools");
+
+// CHAMAR FUNÇOES DELICIAS
+
+//addFiles()
+//makeFolders()
+//moveFiles()
+
+qtdFiles()
+
+function makeFolders(){
+	if(!fs.existsSync(`./autores/sem_autores`)){
+		fs.mkdirSync(`./autores/sem_autores`)	
 	}
 	/** */
-
-}
-
-// move files to your authors	
-for(r in autor){
-	var a = autor[r].nome
-	var file = autor[r].arquivo
-	if(a == '.' || a == '-' || a == '*' || a == '..' || a == ''){
-	//	console.log(a + r)
-		if(fs.existsSync(`./arquivos/${file}`)){
-			fs.renameSync(`./arquivos/${file}`, `./autores/sem_autor/${file}`)
+	for(r in autores){
+		let a = autores[r].nome
+		//a = a.replace('//', '')
+		
+		if(!fs.existsSync(`./autores/${a}`) && authorValido(a)){
+			fs.mkdirSync(`./autores/${a}`)	
+			
 		}
-	} else if(fs.existsSync(`./arquivos/${file}`)){
-		fs.renameSync(`./arquivos/${file}`, `./autores/${a}/${file}`)
-//			
+		/** */
+	
 	}
+	
+}
+function authorValido(a){
+	if(a != '--' && a != '?' && a != '_' && a != '-' && a != '*' && a != '.......... da Mágica' && a != '*.*' && a != '*.*.*.*.*.*.*.*.*.*.*.*.*.*.*' && a != '' && a != '*****' && a != 'A' && a != 'a' && a != 'C' && a != 'c')return true
+	return false
+}
+function moveFiles(){
+	for(r in autores){
+		var a = autores[r].nome
+		var file = autores[r].arquivo
+		if(!authorValido(a)){
+		//	console.log(a + r)
+			if(fs.existsSync(`./arquivos/${file}`)){
+				fs.renameSync(`./arquivos/${file}`, `./autores/sem_autores/${file}`)
+			}
+		}else if(fs.existsSync(`./arquivos/${file}`)){
+			fs.renameSync(`./arquivos/${file}`, `./autores/${a}/${file}`)
+	//			
+		}
+		/** */
+	}
+	
 }
 
-//arquivos qtd
-fs.readdir('./arquivos', (err, files) => {
-	console.log('Qtd de arquivos: '+files.length);
-})
+function qtdFiles(){
+	//arquivos qtd
+	fs.readdir('./arquivos', (err, files) => {
+		console.log('Qtd de arquivos: '+files.length);
+	})
 
-//pastas autores qtd
-fs.readdir('./autores', (err, files) => {
-	console.log('Qtd de arquivos: '+files.length);
-//	console.log(files)
-})
+	//pastas autores qtd
+	fs.readdir('./autores', (err, files) => {
+		console.log('Qtd de Autores: '+files.length);
+	//	console.log(files)
+	})
+}
+function addFiles(){
+	for(r in autores){
+		fs.appendFileSync(`arquivos/${autores[r].arquivo}`, 'teste')
+	}
+	//fs.appendFileSync('teste.pptx', 'teste')
+}
